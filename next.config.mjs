5f95ccf0ea3ea61e59499c05d91468234cf44325
Carlos Pegueros <carlos.pegueros@nubank.com.br>
2024-06-12 22:54:02 -0600

    attempt 3

--- next.config.mjs
+++ next.config.mjs
@@ -16,10 +16,28 @@ const nextConfig = {
 };
 */
 
+/* attempt 2:
 const nextConfig = {
   async rewrites() {
     return {
       afterFiles: [
+        {
+          source: "/api/python/:path*",
+          destination:
+            process.env.NODE_ENV === "development"
+              ? "http://127.0.0.1:5328/api/python/:path*"
+              : "/api/index.py",
+        },
+      ]
+    }
+  },
+};
+*/
+
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
         {
           source: "/api/python/:path*",
           destination:
