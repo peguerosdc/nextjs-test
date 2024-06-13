dca1340c5c545725796b95d07db9118b7dfd65e8
Carlos Pegueros <carlos.pegueros@nubank.com.br>
2024-06-12 22:52:11 -0600

    attempt 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+
+/* attempt 1:
 const nextConfig = {
     async rewrites() {
         return [
@@ -11,6 +13,23 @@ const nextConfig = {
           },
         ];
       },
+};
+*/
+
+const nextConfig = {
+  async rewrites() {
+    return {
+      afterFiles: [
+        {
+          source: "/api/python/:path*",
+          destination:
+            process.env.NODE_ENV === "development"
+              ? "http://127.0.0.1:5328/api/python/:path*"
+              : "/api/index.py",
+        },
+      ]
+    }
+  },
 };
 
 export default nextConfig;
